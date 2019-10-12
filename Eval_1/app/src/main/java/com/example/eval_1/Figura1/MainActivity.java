130da343f1d4b5c941a800134fc057ed20e8884a
Adrian Alba <adrian.alba@outlook.es>
2019-10-12 02:43:55 +0200

    Prepracion Examen_Eval1

---
+++ Eval_1/app/src/main/java/com/example/eval_1/Figura1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.eval_1.Figura1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.eval_1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onPulsado(View view) {
+    }
+}
---
+++ Eval_1/app/src/main/java/com/example/eval_1/Figura2/Main.java
@@ -0,0 +1,13 @@
+package com.example.eval_1.Figura2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
